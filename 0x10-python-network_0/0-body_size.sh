2ee29a38d5ecf289506eec95e4f6f06befa275e7
Ewaboocee <odujinrinbusola02@gmail.com>
2024-05-14 03:11:52 -0700

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-
-# Check if URL is provided as argument
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Send request using curl and get the size of the response body in bytes
-response=$(curl -s -o /dev/null -w "%{size_download}" "$1")
-
-# Display the size of the response body
-echo "Size of the response body: $response bytes"
-
+# A Bash script that takes in a URL, sends a request and displays the size of the body of the response
+curl -sI "$1" | grep "Content-Length:" | cut -d " " -f 2
